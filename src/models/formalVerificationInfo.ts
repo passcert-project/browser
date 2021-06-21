c6761096c7f343d6242f229749b6e68d67bfe2aa
CarolinaCC <carolina.carreira@tecnico.ulisboa.pt>
2021-06-21 18:06:25 +0100

    second iteration after meeting

--- src/models/formalVerificationInfo.ts
+++ src/models/formalVerificationInfo.ts
@@ -5,8 +5,8 @@ export default class FormalVerificationInfo {
 
     static formalVerificationMasterPassword(platformUtilsService: PlatformUtilsService, i18nService: I18nService ) {
         const confirmed = platformUtilsService.showDialog(
-            i18nService.t('formalVerificationMasterPasswordSecurity'), i18nService.t('formalVerification'),
-            i18nService.t('ok'));
+            i18nService.t('formalVerificationMasterPasswordSecurity'), i18nService.t('formalVerificationMasterPasswordSecurityTitle'),
+            i18nService.t('ok'), i18nService.t('learnMore'));
         if (confirmed) {
             // BrowserApi.createNewTab('https://help.bitwarden.com/article/setup-two-step-login/');
         }
@@ -14,8 +14,8 @@ export default class FormalVerificationInfo {
 
     static formalVerificationGenerator(platformUtilsService: PlatformUtilsService, i18nService: I18nService ) {
         const confirmed = platformUtilsService.showDialog(
-            i18nService.t('formalVerificationGenerator'), i18nService.t('formalVerification'),
-            i18nService.t('ok'));
+            i18nService.t('formalVerificationGenerator'), i18nService.t('formalVerificationGeneratorTitle'),
+            i18nService.t('ok'), i18nService.t('learnMore'));
         if (confirmed) {
             // BrowserApi.createNewTab('https://help.bitwarden.com/article/setup-two-step-login/');
         }
@@ -23,8 +23,8 @@ export default class FormalVerificationInfo {
 
     static formalVerificationPasswordSecurity(platformUtilsService: PlatformUtilsService, i18nService: I18nService ) {
         const confirmed = platformUtilsService.showDialog(
-            i18nService.t('formalVerificationPasswordSecurity'), i18nService.t('formalVerification'),
-            i18nService.t('ok'));
+            i18nService.t('formalVerificationPasswordSecurity'), i18nService.t('formalVerificationPasswordSecurityTitle'),
+            i18nService.t('ok'), i18nService.t('learnMore'));
         if (confirmed) {
             // BrowserApi.createNewTab('https://help.bitwarden.com/article/setup-two-step-login/');
         }
